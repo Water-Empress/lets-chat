5bf02097111d62e1a8d468faf7f23dd8b7340627
Water-Empress <110268568+Water-Empress@users.noreply.github.com>
2023-04-11 19:05:28 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,16 +1,16 @@
 
 var firebaseConfig = {
-      apiKey: "AIzaSyAylcYQCK2-5tvZtZSDJYzvDIZioM4laLo",
-      authDomain: "kwitter-6ffe4.firebaseapp.com",
-      databaseURL: "https://kwitter-6ffe4-default-rtdb.firebaseio.com",
-      projectId: "kwitter-6ffe4",
-      storageBucket: "kwitter-6ffe4.appspot.com",
-      messagingSenderId: "782945116172",
-      appId: "1:782945116172:web:6b625261ce8153fe134e29"
+      apiKey: "AIzaSyB_UMkNKG3L97I95g5ONNNczHRa-NiBkVY",
+      authDomain: "let-chat-web-app-652ea.firebaseapp.com",
+      databaseURL: "https://let-chat-web-app-652ea-default-rtdb.firebaseio.com",
+      projectId: "let-chat-web-app-652ea",
+      storageBucket: "let-chat-web-app-652ea.appspot.com",
+      messagingSenderId: "357882244761",
+      appId: "1:357882244761:web:0636f5c3ea006b35829208"
     };
     
-    
-     firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
